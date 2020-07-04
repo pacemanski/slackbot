d9bdf5aac1f72a6508ea2721640e9bbe8f70c830
pcemanski <pacemanski@gmail.com>
2020-07-04 11:22:20 -0300

    Reading token from env config

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const http = require('http');
+require('dotenv').config();
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const SLACK_TOKEN = process.env.SLACK_BOT_TOKEN;
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Alter Bridge rocckss!');
+    res.end('Alter Bridge rocckss! ' + SLACK_TOKEN);
 });
 
 server.listen(port, hostname, () => {
